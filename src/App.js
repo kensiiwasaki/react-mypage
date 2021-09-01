d05cd8b546a0a1a9922fdea659d5256523eaff34
kensiiwasaki <kensiiwasaki@gmail.com>
2021-09-01 16:39:56 +0900

    モーダル完成

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <header>
       <div>
-        <p className="name">岩崎　拳志</p>
+        <p className="name">岩崎拳志</p>
         <p className="furigana">(iwasaki kenshi)</p>
       </div>
       <div>
