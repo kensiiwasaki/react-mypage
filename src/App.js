48d061a215e02e78e0f03b62850e7a204fb1cb50
kensiiwasaki <kensiiwasaki@gmail.com>
2021-09-15 15:21:33 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <div>
         <img src={Icon} alt="アイコン" className="icon" />
         <p className="name">岩崎 拳志</p>
-        <p className="furigana">(iwasaki kenshi)</p>
+        <p className="furigana">iwasaki kenshi</p>
       </div>
       <div className="modal">
         <button className="btn" onClick={() => setShow(true)}>more</button>
